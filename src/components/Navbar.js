d49472572b042e541591a881144cd30bb14db8f9
kananivishal <kananivishal98@gmail.com>
2024-05-27 18:24:10 +0530

    Active Problem Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/TextUtils-React/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/TextUtils-React/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/TextUtils-React/about">{props.aboutText}</Link>
